1c1f16302f7cc2f329cbdefe2a3956cb766c6ab0
ParamSurywashi <parmanandchared1997@gmail.com>
2023-04-10 22:48:16 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(cors());
-app.use(express.static('frontend/build'));
+//app.use(express.static('frontend/build'));
 
 main().catch(err => console.log(err));
 
