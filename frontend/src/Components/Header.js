bd8c9db40700930c9fe046b24abe910c6b4f8f3f
ParamSurywashi <parmanandchared1997@gmail.com>
2023-04-13 20:23:23 +0530

    update nav bar

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -5,7 +5,7 @@ import {MdSearch, MdFoodBank} from "react-icons/md";
 import { IoFastFoodOutline, IoRestaurantSharp } from "react-icons/io5";
 import firstVideo from "../navvideo/firstvid.mp4";
 import secondVideo from "../navvideo/secondvid.mp4";
-import thirdVideo from "../navvideo/thirdvid.mp4";
+import addrecvid from "../navvideo/addrecvid.mp4";
 import searchVideo from "../navvideo/searchvid.mp4"
 
 function Header() {
@@ -20,7 +20,7 @@ function Header() {
        <Link to="/" id='imgLogos'><div><img src="https://cdn.pixabay.com/photo/2012/04/05/00/36/hamburger-25357_960_720.png" /></div> </Link>
       <ul>
         <Link to="/home" className='linkComp' id='fvid' onMouseEnter={()=>setHoveronHome(false)} onMouseLeave={()=>setHoveronHome(true)}>
-          <> <IoRestaurantSharp /> Home </> 
+          <span id='hometxtbox'> <IoRestaurantSharp /> Home </span> 
           {(isHoveronHome) ? ("") : ( <video autoPlay loop muted playsInline className='vidbox'>
              <source src={firstVideo} type="video/mp4"/>
          </video>)}
@@ -36,15 +36,15 @@ function Header() {
       </ul>
       <ul>
         <Link to="/addrecipe" className='linkComp' id='addRecId'onMouseEnter={()=>setHoveronAddRecipe(false)} onMouseLeave={()=>setHoveronAddRecipe(true)}> 
-        <> <IoFastFoodOutline /> Add Recipe </>
+        <span id='addfoodbox'> <IoFastFoodOutline />Add Recipe </span>
         {(isHoveronAddRecipe) ? ("") : ( <video autoPlay loop muted playsInline className='vidbox'>
-             <source src={thirdVideo} type="video/mp4"/>
+             <source src={addrecvid} type="video/mp4"/>
          </video>)}
          </Link>
       </ul>
       <ul>
       <Link to="/search" className='linkComp' id='searchId' onMouseEnter={()=>setHoveronSearchRecipe(false)} onMouseLeave={()=>setHoveronSearchRecipe(true)}> 
-        <> <MdSearch /> Search </>
+        <span id='searchtxtbox'> <MdSearch /> Search </span>
         {(isHoveronSearchRecipe) ? ("") : ( <video autoPlay loop muted playsInline className='vidbox'>
              <source src={searchVideo} type="video/mp4"/>
          </video>)}
