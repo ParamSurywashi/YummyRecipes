55e588399cbd4728e3bd5b5f43c9c3985e5385bf
ParamSurywashi <parmanandchared1997@gmail.com>
2023-04-14 17:06:39 +0530

    updated

--- frontend/src/Components/RecipeCard.js
+++ frontend/src/Components/RecipeCard.js
@@ -2,6 +2,15 @@ import React from 'react'
 import "../styles/RecipeCard.css";
 
 function RecipeCard({recipe}) {
+  const handlereadmore = (e)=>{
+   if(document.getElementsByClassName("readmore")[e.target.id -1].innerText=="--- Read more ---"){
+    document.getElementsByClassName("decrecipe")[e.target.id -1].style.overflowY="auto";
+    document.getElementsByClassName("readmore")[e.target.id -1].innerText="--- Read Less ---";
+   }else{
+    document.getElementsByClassName("decrecipe")[e.target.id -1].style.overflowY="hidden";
+    document.getElementsByClassName("readmore")[e.target.id -1].innerText="--- Read more ---";
+   }
+  }
   return (
     <>
        <h1>{recipe.title}</h1>
@@ -16,6 +25,7 @@ function RecipeCard({recipe}) {
        </ul> */}
        <img src={recipe.img} alt={recipe.title} />
        <p className='decrecipe'>{recipe.recipe}</p>
+       <p className='readmore' id={recipe.reckey} onClick={handlereadmore}>--- Read more ---</p>
     </>
   )
 }
